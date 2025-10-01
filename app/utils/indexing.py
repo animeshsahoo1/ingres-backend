a178846f07dc212affb9b2cfa0b1000b6219836b
animesh sahoo <animeshsahoo159@gmail.com>
2025-10-01 20:01:25 +0530

    created embeddings

---
+++ app/utils/indexing.py
@@ -0,0 +1,35 @@
+from pathlib import Path
+from dotenv import load_dotenv
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_qdrant import QdrantVectorStore
+
+load_dotenv()
+
+pdf_path = Path(__file__).parent.parent.parent / "static" / "GEC_User_manual.pdf"
+
+# Loading
+loader = PyPDFLoader(file_path=pdf_path)
+docs = loader.load()  # Read PDF File
+
+# Chunking
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=400
+)
+
+split_docs = text_splitter.split_documents(documents=docs)
+
+embedding_model = GoogleGenerativeAIEmbeddings(
+    model="models/gemini-embedding-001"
+)
+
+vector_store = QdrantVectorStore.from_documents(
+    documents=split_docs,
+    url="http://localhost:6333",
+    collection_name="ingres_collection",
+    embedding=embedding_model
+)
+
+print("Indexing of Documents Done...")
